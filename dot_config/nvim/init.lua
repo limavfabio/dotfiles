6f190f24541f579d64440b1bdc361946c68ee53c
limavfabio <limavfabio@gmail.com>
2026-02-04 16:22:16 -0300

    nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,13 @@
 vim.keymap.set( "i", "jk", "<esc>")
 vim.keymap.set( "i", "kj", "<esc>")
+
+vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>")
+
+vim.opt.clipboard = "unnamedplus"   -- Sync with system clipboard (no more "+y)
+vim.opt.undofile = true            -- Persistent undo (survives closing Nvim)
+vim.opt.smartcase = true           -- Smart case-sensitive search
+vim.opt.inccommand = "split"       -- Live preview of :s/replace/commands
+vim.opt.cursorline = true          -- Highlight current line for orientation
+vim.opt.scrolloff = 8              -- Keep 8 lines visible above/below cursor
+
+vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank() end })
